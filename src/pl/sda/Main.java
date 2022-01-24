cd33b6819449a2bbdd136fa92aac0dd692e485fb
Cezary Kochański <cezarykochanski@Air-Cezary.wtvk.pl>
2022-01-24 18:42:18 +0100

    Fix numbers counting

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Printing numbers from 1 to 10");
-        IntStream.range(0, 10).forEach(System.out::println);
+        IntStream.rangeClosed(1, 10).forEach(System.out::println);
     }
 }
